fd8fed7adf0672af3e58e54f5b245808106ed9da
Michael Tomcal <mtomcal@cbtnuggets.com>
2016-04-14 14:56:29 -0700

    Init commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'scss?sourceMap']
+      },
+      {
+        test: /\.(png|jpg|woff|otf|woff2|eot|ttf|svg)+$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  }
+};
